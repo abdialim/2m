671b4e4c38ccc85ecd0b596af500585627070afa
abdialim <167313831+abdialim@users.noreply.github.com>
2024-04-26 17:42:30 +0600

    Add files via upload

---
+++ Новая папка (7)/style.js
@@ -0,0 +1,86 @@
+const fjf = [1, 2, 3, 4, 5, 6]
+// const users = ['alim','bael','erik','peri']
+// console.log(users [2] );
+
+
+
+
+
+
+
+
+// for (let i = 0; i <=100; i++) {
+//     if ( i % 2 == 0){
+//       console.log('salam popalam' +i );   
+//     }
+   
+// }
+
+
+
+// const numbers = [2, 2, 3, 4, 5 ,6 ,7,85, 56]
+// console.log(numbers);
+// for(let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] *2);
+// }
+
+
+// let num = 1
+// num = num +3
+// console.log(num);
+
+// let num = [1, 2, 3, 4, 5, 6, 7 ]
+// num = num +num
+// console.log(num);
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7 ]
+
+// let sum = 0
+
+// for(let i = 0; i < numbers.length; i++) {
+//      constelement = numbers[i] ;
+//      sum += element
+//     }
+//     console.log(sum);
+
+// const array = [1, 2, 3, 4,55555, 5, 6, 7]
+
+// for (let i = 0; i < array.lengt; i++) {
+//     const element = array = [i];
+//     if (element > maxNumber) {
+//         maxNumber = array [i]
+//     }
+    
+    
+// }
+// console.log(maxNumber);
+ 
+
+
+
+
+let values = [100, 55, 15, 300, 20];
+
+let sum = 0;
+
+
+for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+}
+
+console.log("Сумма значений массива:", sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
